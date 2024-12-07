8798a207701cf91a0ab0f700f12be9c297251261
Vishnu Suthar <sutharvishnu511@gmail.com>
2024-12-07 21:27:08 +0530

    Update Implement_Caesar_Cipher.py

--- PRODIGY_WD_01/Implement_Caesar_Cipher.py
+++ PRODIGY_WD_01/Implement_Caesar_Cipher.py
@@ -1,1 +1,33 @@
+def encrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+
+def decrypt(text, shift):
+    return encrypt(text, -shift)
+
+
+def main():
+    choice = input("Do you want to (e)ncrypt or (d)ecrypt? ")
+    text = input("Enter your message: ")
+    shift = int(input("Enter shift value: "))
+
+    if choice.lower() == 'e':
+        print("Encrypted message:", encrypt(text, shift))
+    elif choice.lower() == 'd':
+        print("Decrypted message:", decrypt(text, shift))
+    else:
+        print("Invalid choice")
+
+
+if __name__ == "__main__":
+    main()
 

